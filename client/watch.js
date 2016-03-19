f70f24ece9c6e09d5c8876df7afbefca8ce27e76
Reage <rentiansheng@163.com>
2016-03-19 15:21:02 +0800

    add devSync all file

--- client/watch.js
+++ client/watch.js
@@ -5,53 +5,92 @@ var net = require('net');
 
 
 
+
+
 var fileWatcher = (function() {
+
+  function sendFile(dir, filename, item, display ) {
+
+    if(filename && filename[0] == '.') return;
+    var ext = path.extname(filename);
+    if(item.exts && item.exts.indexOf(ext) === -1) return;
+    fs.readFile(dir+'/'+filename, function (err, content) {
+      if(err) { return false;}
+      var relativePath = path.relative(item.offline, dir);
+      relativePath = relativePath.replace(/\\/g, '/');
+      var servfile  = item.online +'/'+relativePath+'/'+filename;
+      var start =  new Date().getTime();
+
+      try{
+        if(display == 1) {
+          console.log('data:'+content.length);
+        }
+        var client = net.connect(
+            {host: item.host, port: item.port},
+            function() { //'connect' listener
+              client.write('put '+servfile+'\n'+content.length+'\n\n'+content);
+            }
+        );
+
+        client.on('data', function(data) {
+          client.end();
+        });
+        client.on('end', function() {
+          client.end();
+          if(display == 1 ) {
+            console.log('end: '+servfile);
+            var end = new Date().getTime();
+            var ts = end - start;
+            console.log('end: '+content.length);
+            console.log('end: start: '+ start+' end: '+ end +' ts:'+ts);
+          }
+
+        });
+      }catch(err) {
+        console.log(err);
+      }
+
+    });
+  }
+
   function listenToChange(dir, item) {
     dir = path.resolve(dir);
     function onChg (event, filename) {
-
-      if(filename && filename[0] == '.') return;
-      var ext = path.extname(filename);
-      if(item.exts && item.exts.indexOf(ext) === -1) return;
-      fs.readFile(dir+'/'+filename, function (err, content) {
-        if(err) { return false;}
-        var relativePath = path.relative(item.offline, dir);
-        relativePath = relativePath.replace(/\\/g, '/');
-        var servfile  = item.online +'/'+relativePath+'/'+filename;
-        var start =  new Date().getTime();
-
-        try{
-          console.log('data:'+content.length);
-          var client = net.connect(
-              {host: item.host, port: item.port},
-              function() { //'connect' listener
-                console.log('connect: '+servfile);
-
-                client.write('put '+servfile+'\n'+content.length+'\n\n'+content);
-              }
-          );
-
-          client.on('data', function(data) {
-            client.end();
-          });
-          client.on('end', function() {
-            client.end();
-            var end = new Date().getTime();
-            var ts = end - start;
-            console.log('end: '+servfile);
-            console.log('end: '+content.length);
-            console.log('end: start: '+ start+' end: '+ end +' ts:'+ts);
-          });
-        }catch(err) {
-          console.log(err);
-        }
-
-      });
+      sendFile(dir, filename, item,1);
     }
 
     fs.watch(dir, onChg);
   }
 
+  function syncAllFILE(root,item) {
+
+    fs.lstat(root, function (err, stats) {
+      if (stats.isDirectory()) {
+        fs.readdir(root, function (err, files) {
+          if (err) return;
+          files.forEach(function (file) {
+            if(file[0] == '.'){ return ;}
+            file = root + '/' + file;
+
+            fs.lstat(file, function (err, stats) {
+
+
+              if (err) return;
+              if (stats.isDirectory()) {
+                syncAllFILE(file, item);
+              }else if(stats.isFile()) {
+                file  = path.parse(file);
+                if(!file['base']) {return;}
+                sendFile(root,file['base'],item, 0);
+              }
+            });
+          });
+        });
+
+      }
+
+    });
+  }
 
 
   function watchDir(root, item) {
@@ -61,6 +100,7 @@ var fileWatcher = (function() {
         fs.readdir(root, function (err, files) {
           if (err) return;
           files.forEach(function (file) {
+            fileName = file;
             if(file[0] == '.'){ return ;}
             file = root + '/' + file;
             fs.lstat(file, function (err, stats) {
@@ -83,18 +123,24 @@ var fileWatcher = (function() {
     var options = process.argv
     var len = options.length;
     var i ;
-    var argv = {'d':'','v':''}
+    var argv = {'d':'','v':'','devSyncAll':false}
 
     for(i = 0; i< len; i++ ) {
       if(options[i] == '-d') {
         argv.d = options[++i];
+      }else if(options[i] == 'all') {
+        argv.devSyncAll = true;
       }
     }
+
+
 
     if(argv.d == null) {
       console.log("error: 请输入config配置中path配置项目");
       return ;
     }
+
+
     if(argv.d != '') {
       var pwd = process.cwd();
       console.dir(pwd);
@@ -107,7 +153,15 @@ var fileWatcher = (function() {
         if(!item.port) {
           item.port = config.server.port;
         }
+        item.devSyncAll = argv.devSyncAll;
 
+        if(argv.devSyncAll) {
+          syncAllFILE(item.offline, item);
+          console.log("\n-------------------------------------------------\n");
+          console.log("\n  devSync all end\n");
+          console.log("\n-------------------------------------------------\n");
+
+        }
         watchDir(item.offline, item);
       } else {
         console.log("error:"+argv.d+"配置不存在");
@@ -122,6 +176,7 @@ var fileWatcher = (function() {
           if(!item.port) {
             item.host = config.server.port;
           }
+          item.devSyncAll = argv.devSyncAll;
           watchDir(item.offline, item);
         }
       });
