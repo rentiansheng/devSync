aa2c9d9d563af732202e8b5b981657a9a8132053
Reage <rentiansheng@163.com>
2016-10-25 12:21:14 +0800

    hash

--- server/core/hash.c
+++ server/core/hash.c
@@ -1,119 +1,258 @@
-#include "hash.c"
+#include "hash.h"
 
-hash * hash_init(pool_t *p) {
-    hash *h = NULL;
+hash_t * hash_init(pool_t *p) {
+    hash_t *h = NULL;
 
-    h =(hash  *) palloc(p, sizeof(hash));
+    h =(hash_t  *) palloc(p, sizeof(hash_t));
     h->buckets = NULL;
     h->size = HASH_DEFAULT_SIZE;
     h->count = 0;
     h->mask = h->size - 1;
-    h->buckets = (bucket *)pcalloc(p, sizeof(bucket)); 
+    h->buckets = (bucket_t *)pcalloc(p, sizeof(bucket_t)*h->size); 
 
     return h;
 }
 
 
 
-int hash_add_int(pool_t *p, const *key, int keyLen, int  v) {
+int hash_add_int(pool_t *p, hash_t  *h, const char  *key, int keyLen, int  v) {
+    return hash_add(p, h ,key, keyLen, &v, HASH_ITEM_VALUE_TYPE_INT);
 }
 
-static int hash_add(pool_t *p, hash *h ,const char * key, int keyLen, void * ptr, int type) {
-    unsigned int index = hash_index(char *key, keyLen);
+int hash_add_long(pool_t *p, hash_t *h, const char *key, int keyLen, long int  v) {
+    return hash_add(p, h ,key, keyLen, &v, HASH_ITEM_VALUE_TYPE_LONG);
+}
 
-    item * newItem = (item *)pcalloc(p, sizeof(item));
-    newItem->key = string_init_from_ptr(key, keyLen);
+int hash_add_double(pool_t *p, hash_t *h, const char *key, int keyLen, double v) {
+    return hash_add(p, h ,key, keyLen, &v, HASH_ITEM_VALUE_TYPE_DOUBLE);
+}
+
+int hash_add_string(pool_t *p, hash_t *h, const char *key, int keyLen, string *v) {
+    return hash_add(p, h ,key, keyLen, v, HASH_ITEM_VALUE_TYPE_STRING);
+}
+
+int hash_add_ptr(pool_t *p, hash_t *h, const char *key, int keyLen, void * v) {
+     return hash_add(p, h ,key, keyLen, v, HASH_ITEM_VALUE_TYPE_PTR);
+}
+
+int hash_get_int(hash_t *h, const char *key, int keyLen,int * ret) {
+
+    hash_item_t * item  = get_hash_item(h, key, keyLen);
+
+    if(item == NULL || item->type != HASH_ITEM_VALUE_TYPE_INT) {
+        return -1;
+    }
+
+    *ret = item->value.i;
+
+    return 0;
+}
+
+int hash_get_long(hash_t *h, const char *key, int keyLen, long * ret) {
+    hash_item_t * item  = get_hash_item(h, key, keyLen);
+
+    if(item == NULL || item->type != HASH_ITEM_VALUE_TYPE_LONG) {
+        return -1;
+    }
+
+    *ret = item->value.l;
+
+    return 0;
+}
+
+int hash_get_double(hash_t *h, const char *key, int keyLen, double * ret) {
+     hash_item_t * item  = get_hash_item(h, key, keyLen);
+
+    if(item == NULL || item->type != HASH_ITEM_VALUE_TYPE_DOUBLE) {
+        return -1;
+    }
+
+    *ret = item->value.d;
+
+    return 0;
+}
+
+int hash_get_string(hash_t *h, const char *key, int keyLen, string * ret) {
+    hash_item_t * item  = get_hash_item(h, key, keyLen);
+
+    if(item == NULL || item->type != HASH_ITEM_VALUE_TYPE_STRING) {
+        return -1;
+    }
+
+    memcpy(ret, item->value.str, sizeof(string));
+
+    return 0;
+}
+
+int hash_get_ptr(hash_t *h, const char *key, int keyLen, void * ret, size_t size) {
+     hash_item_t * item  = get_hash_item(h, key, keyLen);
+
+    if(item == NULL || item->type != HASH_ITEM_VALUE_TYPE_PTR) {
+        return -1;
+    }
+
+    memcpy(ret, item->value.ptr, size);
+
+    return 0;
+}
+
+int hash_del_item(hash_t *h, const char *key, int keyLen) {
+
+    int index = hash_index(h, key, keyLen);
+    hash_item_t *pre, *cur ;
+    pre = NULL;
+    cur = h->buckets[index].items;
+    if(h->buckets[index].count == 0 || cur == NULL) {
+        return 0;
+    }
+    while(cur) {
+        if(keyLen == cur->key->len && strncmp(key, cur->key->ptr, keyLen) == 0) {
+            break;
+        }
+        pre = cur;
+        cur = cur->next;
+    }
+    if(cur != NULL) {
+        if(pre == NULL) {
+            h->buckets[index].items = cur->next;
+        } else {
+            pre->next = cur->next;
+        }
+        h->buckets[index].count--;
+    }
+    
+
+    return 0;
+}
+
+
+static hash_item_t * get_hash_item(hash_t *h, const char *key, int keyLen) {
+    unsigned int index = hash_index(h, key, keyLen);
+
+    hash_item_t * item = h->buckets[index].items;
+    if(h->buckets[index].count == 0 || item == NULL) {
+        return NULL;
+    }
+
+    while(item ) {
+        if(strncmp(key, item->key->ptr, keyLen) == 0) {
+            return item;
+        }
+        item = item->next;
+    }
+
+    return NULL;
+
+}
+
+
+static int hash_add(pool_t *p, hash_t *h ,const char * key, int keyLen, void * ptr, int type) {
+
+    hash_item_t * newItem = (hash_item_t *)pcalloc(p, sizeof(hash_item_t));
+    newItem->key = string_init_from_ptr(p, key, keyLen);
     newItem->type = type;
     newItem->next = NULL;
+
+    if((h->count+1) > h->size) {
+        int reint = hash_reinit(p, h);
+        if(reint) {
+            return reint;
+        }
+    }
 
     switch(type) {
         case HASH_ITEM_VALUE_TYPE_INT:
         newItem->value.i = *(int*)ptr;
         break;
         case HASH_ITEM_VALUE_TYPE_LONG:
-        newItem->value.l = *(long int)ptr;
+        newItem->value.l = *(long int*)ptr;
         break;
         case HASH_ITEM_VALUE_TYPE_DOUBLE:
-        newItem->v.d = *(long int)ptr;
+        newItem->value.d = *(double*)ptr;
         break;
         case HASH_ITEM_VALUE_TYPE_STRING:
         newItem->value.str = (string *) ptr;
+        break;
+        case HASH_ITEM_VALUE_TYPE_PTR:
+        newItem->value.ptr = (void *) ptr;
         break;
         default:
         return -1;
     }
 
-    
-    if(h->count/h->size) {
-        int reint = hash_reinit(pool_t *p, hash *h);
-        if(reint) {
-            return reint;
-        }
-    }
-    index = index&h->mask;
-
-    if((h->buckets[index]).count == 0) {
-
-        (h->buckets[index]).items = newItem;
-    }else {
-        item i = (h->buckets[index]).item;
-        while(i->next != null) {
-            i = i->next;
-        }
-        i->next = newItem;
-    }
-    (h->buckets[index]).count += 1;
-    h->count += 1;
+    hash_add_item(h, newItem);
 
 
     return 0;
 
 }
 
-static int hash_add_item(pool_t *p, hash *h, item *i) {
-    unsigned int index = hash_init(i->key.ptr, i->key.len);
-    
-    index = index&h->mask;
+static int hash_add_item(hash_t *h, hash_item_t *newItem) {
+
+    unsigned int index = hash_index(h, newItem->key->ptr, newItem->key->len);
 
 
+    hash_item_t *i = (h->buckets[index]).items;
+    int isAdd = 1;
+    while(i) {
+        if(string_compare(i->key, newItem->key) == 0) {
+            isAdd = 0;
+            i->type = newItem->type;
+            i->value = newItem->value;
+            break;
+        }
+        i = i->next;
+    }
+    if(isAdd) {
+        (h->buckets[index]).count += 1;
+        h->count += 1;
+        newItem->next = h->buckets[index].items;
+        h->buckets[index].items = newItem;
+    }
+        
+    return 0;
 }
 
-static unsigned int  hash_reinit(pool_t *p, hash *h) {
+static unsigned int  hash_reinit(pool_t *p, hash_t *h) {
 
-    hash *rehash = NULL;
+    hash_t *rehash = NULL;
     int i = 0;
-    item * hash_item;
+    hash_item_t * item;
+    hash_item_t *next;
 
-    rehash =(hash  *) palloc(p, sizeof(hash));
+    rehash =(hash_t  *) palloc(p, sizeof(hash_t));
     rehash->buckets = NULL;
-    rehash->size = (rehash->size)<<1;
+    rehash->size = (h->size)<<1;
     rehash->count = 0;
     rehash->mask = rehash->size - 1;
-    rehash->buckets = (bucket *)pcalloc(p, sizeof(bucket));
+    rehash->buckets = (bucket_t *)pcalloc(p, sizeof(bucket_t)*rehash->size);
 
-    for(i = 0; i < h.count; i++){
-        hash_item = h->bucket[i];
-        while(hash_item != NULL) {
-            hash_add_item(p, rehash, hash_item);
-            hash_item = hash_item->next;
+    for(i = 0; i < h->size; i++){
+        item = h->buckets[i].items;
+        while(item) {
+            next = item->next;
+            hash_add_item(rehash, item);
+            item = next;
         }
     }
 
-    memcpy(h, rehash, sizeof(hash));
+    memcpy(h, rehash, sizeof(hash_t));
 
-    return h;
+    return 0;
 }
 
-
-
-
-static unsigned int hash_index(char *ptr, int len) {
+static unsigned int hash_index(hash_t * h, char *ptr, int len) {
     unsigned int index = 0;
     int i = 0;
     while(i < len) {
         index = index + (ptr[i]<<5) + ptr[i];
+        i++;
     }
+
+    index = index&h->mask;
     
     return index;
 
 }
+
+
--- server/core/hash.h
+++ server/core/hash.h
@@ -8,7 +8,7 @@
 #include <stdlib.h>
 #include "pool.h"
 #include "str.h"
-#include "memeory.h"
+#include <string.h>
 
 //必须是2的幂
 #define HASH_DEFAULT_SIZE 8
@@ -16,40 +16,56 @@
 #define HASH_ITEM_VALUE_TYPE_LONG 2
 #define HASH_ITEM_VALUE_TYPE_DOUBLE 3
 #define HASH_ITEM_VALUE_TYPE_STRING 4
+#define HASH_ITEM_VALUE_TYPE_PTR 5
 
 
 
-
-union item_value {
+typedef union hash_item_value {
     int i;
     long int l;
     double d;
     string * str;
-};
+    void * ptr;
+}hash_item_value_u;
 
 
-typedef struct item {
+typedef struct hash_item {
     string * key;
     int type;
-    item_value value;
-    item * next;
+    hash_item_value_u value;
+    struct hash_item * next;
 
-}item;
+}hash_item_t;
 
 typedef struct bucket{
     size_t count;
-    item * items;
-}bucket;
+    hash_item_t * items;
+}bucket_t;
 
 typedef struct hash{
     size_t count;
     size_t size;
     size_t mask;
-    bucket *buckets;
-}hash;
+    bucket_t *buckets;
+}hash_t;
 
-hash hash_init(pool_t *p);
+hash_t * hash_init(pool_t *p);
+int hash_add_int(pool_t *p, hash_t *h, const char *key, int keyLen, int  v) ;
+int hash_add_long(pool_t *p,hash_t *h, const char *key, int keyLen, long  v);
+int hash_add_double(pool_t *p, hash_t *h,const char *key, int keyLen, double v);
+int hash_add_string(pool_t *p,hash_t *h, const char *key, int keyLen, string *v);
+int hash_add_ptr(pool_t *p, hash_t *h, const char *key, int keyLen, void * v);
+int hash_get_int(hash_t *h, const char *key, int keyLen, int * ret);
+int hash_get_long(hash_t *h, const char *key, int keyLen, long int * ret);
+int hash_get_double(hash_t *h, const char *key, int keyLen, double * ret);
+int hash_get_string(hash_t *h, const char *key, int keyLen, string * ret);
+int hash_get_ptr(hash_t *h, const char *key, int keyLen, void * ret, size_t size);
+int hash_del_item(hash_t *h, const char *key, int keyLen);
+static int hash_add_item(hash_t *h, hash_item_t *newItem);
+static int hash_add(pool_t *p, hash_t *h ,const char * key, int keyLen, void * ptr, int type);
+static hash_item_t * get_hash_item(hash_t *h, const char *key, int keyLen);
+static unsigned int  hash_reinit(pool_t *p, hash_t *h);
 
-static unsigned int hash_index(char *ptr, int len);
+static unsigned int hash_index(hash_t * h, char *ptr, int len);
 
 #endif
