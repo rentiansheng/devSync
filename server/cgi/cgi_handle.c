caaed8dad117efb7b508e36c91da7f90031ef3f8
Reage <rentiansheng@163.com>
2016-11-01 22:59:07 +0800

    write execute file error to file

--- server/cgi/cgi_handle.c
+++ server/cgi/cgi_handle.c
@@ -21,6 +21,7 @@ void cgi_handle(epoll_cgi_t *cgi_info, http_conf *g) {
 	cgi_ev_t *cgi_ev;
 	pool_t *p  = pool_create();
 	char *argv[3] = {0};
+	int d = 0;
 	int infd[2], outfd[2];//infd server read, outfd server write;
 
 	cgi_ev = (cgi_ev_t *)pcalloc(p, sizeof(cgi_ev_t));
@@ -32,26 +33,30 @@ void cgi_handle(epoll_cgi_t *cgi_info, http_conf *g) {
 	cgi_ev->count = 0;
 	add_envp(p, cgi_ev, "REQUEST_METHOD" , "execve");
 	
-	argv[0] = "/bin/sh";
+
+	argv[0] = "/bin/bash";
 	argv[1] = cgi_info->file->ptr;
 	argv[2] = 0;
 	//argv[0] = (char *)"/www/aa.cgi";
 
 	int pid = fork();
-	
+
 	switch(pid) {
 		case -1: 
 			break;
 		case 0: 
+	
 			dup2(infd[1], STDOUT_FILENO);
+			dup2(outfd[0], STDIN_FILENO);
+			dup2(infd[1], STDERR_FILENO);
 			
 			close(infd[0]);
 			close(outfd[1]);
-			if(execve("/bin/sh", argv, environ) == 0) {
+
+			if(execve(argv[0], argv, environ) == 0) {
 			}
 			else {
 			}
-
 			close(infd[1]);
 			close(outfd[0]);
 			exit(0);
@@ -74,8 +79,13 @@ void cgi_handle(epoll_cgi_t *cgi_info, http_conf *g) {
 			extra = (epoll_extra_data_t *) pcalloc(p, sizeof(epoll_extra_data_t));
 			extra->type = CGIFD;
 			extra->ptr = (void *) cgi_info;
+			make_fd_non_blocking(cgi_info->pipe.in);
+			make_fd_non_blocking(cgi_info->pipe.out);
 
-			epoll_add_fd(g->epfd, infd[0], EPOLL_R, (void *)extra);	
+
+			epoll_add_fd(g->epfd, cgi_info->pipe.in, EPOLL_R, (void *)extra);	
+			epoll_add_fd(g->epfd, cgi_info->pipe.out, EPOLL_R, (void *)extra);	
+
 			break;
 	}
 
@@ -87,7 +97,7 @@ int init_cgi_data_struct(string *execute_file, execute_cgi_info_manager_t *cgi_i
 	epoll_cgi_t * cgiInfo = (epoll_cgi_t *)palloc(cgi_info_manager->p, sizeof(epoll_cgi_t ));
 	int nameSize = 30;
 	char *name =(char *)palloc(cgi_info_manager->p,nameSize); //存储编译输出内容的文件
-	snprintf(name, nameSize, "/tmp/devsync%d.out", ts);
+	snprintf(name, nameSize, "/tmp/devsync%d.out", 888);//ts);
 	
 	cgiInfo->file = string_init_from_str(cgi_info_manager->p, execute_file->ptr, execute_file->len);
 	cgiInfo->last_add_ts = ts;
@@ -101,11 +111,13 @@ int init_cgi_data_struct(string *execute_file, execute_cgi_info_manager_t *cgi_i
 int get_cgi_operator_handle(epoll_cgi_t *cgi_info) {
 	int size = 1024;
 	int count ;
-	char content[1024];
+	char content[1025];
 	
 
 	 while((count = read(cgi_info->pipe.in, content, size)) > 0) {
 		write(cgi_info->fd, content, count);
+		content[count] = 0;
+		printf("%s\n", content);
     }
 
 }
