747411c125e4ed076b1d192b609b73bfc1747771
rentiansheng <rentiansheng@163.com>
2020-04-01 20:59:59 +0800

    feauter: plan support sync all befor clean and end build, no work, wait doing. is back

--- client/watch.js
+++ client/watch.js
@@ -5,13 +5,13 @@ var net = require('net');
 
 
 
-var fileWatcher = (function() {
+var fileWatcher = (function () {
 
     var count = 0;
     var allfile = [];
     var allFileCount = 0;
     var sendFileNo = 0;
-    var MaxFileSize = 14*1024*1024; //14m
+    var MaxFileSize = 14 * 1024 * 1024; //14m
     var SYNC_METHOD_UPLOAD = 'upload';
     var SYNC_METHOD_DELETE = 'delete';
     var maxProcess = 20;
@@ -19,11 +19,11 @@ var fileWatcher = (function() {
 
 
 
-  //发送文件
+    //发送文件
     function sendFile(dir, filename, item) {
         if (filename && filename[0] == '.') return;
-        var ext = path.extname(filename);
-        if (item.exts && item.exts.length > 0 && item.exts.indexOf(ext) === -1) {return;}
+        /*var ext = path.extname(filename);
+        if (item.exts && item.exts.length > 0 && item.exts.indexOf(ext) === -1) { return; }*/
 
         var relativePath = path.relative(item.offline, dir);
         relativePath = relativePath.replace(/\\/g, '/');
@@ -31,9 +31,9 @@ var fileWatcher = (function() {
         var localFile = dir + '//' + filename;
         localFile = localFile.replace('//', "/")
 
-        for (var i = 0; i < item.ignore.length; i++) { 
-            if (localFile.indexOf(item.ignore[i]) >= 0) { 
-               return 
+        for (var i = 0; i < item.ignore.length; i++) {
+            if (localFile.indexOf(item.ignore[i]) >= 0) {
+                return
             }
         }
         var stat = fs.statSync(localFile);
@@ -44,8 +44,9 @@ var fileWatcher = (function() {
 
     function sendDelFile(dir, filename, item) {
         if (filename && filename[0] == '.') return;
-        var ext = path.extname(filename);
-        if (ext.length > 0 && item.exts && item.exts.length > 0 && item.exts.indexOf(ext) === -1) { return; }
+        /*var ext = path.extname(filename);
+        // 判断是否设定只同步固定后缀的文件
+        if (ext.length > 0 && item.exts && item.exts.length > 0 && item.exts.indexOf(ext) === -1) { return; }*/
 
         var relativePath = path.relative(item.offline, dir);
         relativePath = relativePath.replace(/\\/g, '/');
@@ -63,18 +64,20 @@ var fileWatcher = (function() {
         dir = path.resolve(dir);
 
         function onChg(event, filename) {
-            if(filename.indexOf('___jb_tmp___') >= 0
-              || filename.indexOf('___jb_old___') >= 0) {
-                return ;
+            if (filename.indexOf('___jb_tmp___') >= 0
+                || filename.indexOf('___jb_old___') >= 0) {
+                return;
             }
             var file = dir + '/' + filename;
-            fs.exists(file, function(exists) {
+            fs.exists(file, function (exists) {
                 if (!exists) {
-
+                    var ext = path.extname(filename);
+                    // 判断是否设定只同步固定后缀的文件
+                    if (ext.length > 0 && item.exts && item.exts.length > 0 && item.exts.indexOf(ext) === -1) { return; }
                     sendDelFile(dir, filename, item);
                     return;
                 }
-                fs.lstat(dir + '/' + filename, function(err, stats) {
+                fs.lstat(dir + '/' + filename, function (err, stats) {
                     if (err) {
                         return;
                     }
@@ -86,6 +89,9 @@ var fileWatcher = (function() {
                         watchDir(dir + '/' + filename, item, isSendFile);
                     } else if (stats.isFile()) {
                         if (stats.size < item.maxFileSize) {
+                            var ext = path.extname(filename);
+                            // 判断是否设定只同步固定后缀的文件
+                            if (ext.length > 0 && item.exts && item.exts.length > 0 && item.exts.indexOf(ext) === -1) { return; }
                             sendFile(dir, filename, item);
                         } else {
                             console.log("\n\n\n     You can not synchronize large files! \n\n\n");
@@ -114,21 +120,21 @@ var fileWatcher = (function() {
         dirPath.push(root)
 
         //采用非递归方式监控目录变化，避免callback 层级过多
-        while(0 < dirPath.length) {
-          var dir = dirPath.pop(dirPath);
+        while (0 < dirPath.length) {
+            var dir = dirPath.pop(dirPath);
 
-          listenToChange(dir, item);
-          var files = fs.readdirSync(dir);
-          for(var i = 0; i < files.length; i++) {
-            var file = files[i];
+            listenToChange(dir, item);
+            var files = fs.readdirSync(dir);
+            for (var i = 0; i < files.length; i++) {
+                var file = files[i];
 
-            if (file[0] == '.') { continue; }
-            file = dir + '/' + file;
-            var stats = fs.lstatSync(file);
-            if (stats.isDirectory()) {
-              dirPath.push(file);
+                if (file[0] == '.') { continue; }
+                file = dir + '/' + file;
+                var stats = fs.lstatSync(file);
+                if (stats.isDirectory()) {
+                    dirPath.push(file);
+                }
             }
-          }
 
         }
 
@@ -163,6 +169,8 @@ var fileWatcher = (function() {
                 if (isSkip) {
                     continue;
                 }
+
+
                 try {
                     stats = fs.lstatSync(file);
                     if (stats.isDirectory()) {
@@ -173,11 +181,13 @@ var fileWatcher = (function() {
                     } else if (stats.isFile()) {
                         file = path.parse(file);
                         if (!file['base']) { continue; }
+                        var ext = path.extname(filename);
+                        if (item.exts && item.exts.length > 0 && item.exts.indexOf(ext) === -1) { continue };
                         allfile[allFileCount++] = { 'dir': dir, 'name': file['base'], 'item': item };
                     }
                 } catch (err) {
                     console.log("\n error:" + err.toString())
-                    console.log("file:"+ dir + "/" + file);
+                    console.log("file:" + dir + "/" + file);
                 }
 
             }
@@ -192,32 +202,35 @@ var fileWatcher = (function() {
     //同步全部文件
     function syncAllFile(rootItem) {
 
-      var syncItem = allfile.pop();
-      if (syncItem !== undefined && syncItem ) {
-
+        var syncItem = allfile.pop();
+        if (syncItem !== undefined && syncItem) {
             sendFileForSyncAll(syncItem.dir, syncItem.name, syncItem.item, rootItem, 0);
             syncItem = undefined;
-            console.info("start sync file process: "+(++sendFileNo)+"/"+allFileCount);
+            console.info("start sync file process: " + (++sendFileNo) + "/" + allFileCount);
         } else {
-            childProcess --;
-            if (childProcess <=  0) {
+
+            childProcess--;
+            if (childProcess <= 0) {
                 printSyncAllHeader(false);
                 //不同步目录下文件
                 watchDir(item.offline, item);
 
-              return;
+                return;
             }
         }
 
         return;
     }
 
-    var sendFileCount = {'succ':0, 'error':0};
+    var sendFileCount = { 'succ': 0, 'error': 0 };
     //用于同步全部文件时发送文件
     function sendFileForSyncAll(dir, filename, item, rootItem, retry) {
         if (filename && filename[0] == '.') { syncAllFile(rootItem); return; };
-        var ext = path.extname(filename);
-        if (item.exts && item.exts.length > 0 && item.exts.indexOf(ext) === -1) { syncAllFile(rootItem); return };
+
+        var canExecsh = false
+        if (allfile.length == 0) {
+            canExecSh = true
+        }
 
         var relativePath = path.relative(item.offline, dir);
         relativePath = relativePath.replace(/\\/g, '/');
@@ -227,36 +240,40 @@ var fileWatcher = (function() {
         var stat = fs.statSync(localFile);
         var client = client = net.connect({ host: item.host, port: item.port },
 
-          function() { //'connect' listener
-              client.write('put ' + servfile + '\ncontent-length:' + stat.size + '\n\n');// + content);
-              var fileStream = fs.createReadStream(localFile);
-              fileStream.pipe(client);
+            function () { //'connect' listener
+                if (canExecsh) {
+                    client.write('put ' + servfile + '\ncontent-length:' + fileSize + '\n\nexecute-file:' + item.sh + '\n\n');// + content);
+                } else {
+                    client.write('put ' + servfile + '\ncontent-length:' + fileSize + '\n\n');// + content);
+                }
+                var fileStream = fs.createReadStream(localFile);
+                fileStream.pipe(client);
 
-          }
+            }
         );
 
-        client.on('error', function(error) {
+        client.on('error', function (error) {
             isErr = true;
             if (retry < 3) {
                 sendFileForSyncAll(dir, filename, item, rootItem, retry + 1);
             } else {
-                setTimeout(function() {syncAllFile(rootItem);}, 0)
+                setTimeout(function () { syncAllFile(rootItem); }, 0)
                 //syncAllFile(rootItem);
                 console.log('error:' + servfile);
             }
         });
 
-        client.on('data', function(data) {
+        client.on('data', function (data) {
             client.end();
 
         });
-        client.on('end', function() {
-            if(isErr == false) {
-                sendFileCount['succ'] ++;
+        client.on('end', function () {
+            if (isErr == false) {
+                sendFileCount['succ']++;
                 syncAllFile(rootItem);
 
             } else {
-                sendFileCount['error'] ++;
+                sendFileCount['error']++;
             }
 
             client.end();
@@ -266,7 +283,7 @@ var fileWatcher = (function() {
 
     function syncAllStartProcess(rootItem) {
         childProcess = maxProcess;
-        for(var i = 0; i < maxProcess; i++) {
+        for (var i = 0; i < maxProcess; i++) {
             syncAllFile(rootItem);
         }
     }
@@ -278,7 +295,7 @@ var fileWatcher = (function() {
 
         var client = net.connect({ host: item.host, port: item.port });
 
-        client.on('data', function(data) {
+        client.on('data', function (data) {
             //console.log(data.toString());
             if (isErr == false) {
                 var arrRespone = [] // = data.toString()
@@ -294,7 +311,7 @@ var fileWatcher = (function() {
                 }
             }
         });
-        client.on('end', function() {
+        client.on('end', function () {
             if (isErr) {
                 return
             }
@@ -304,7 +321,7 @@ var fileWatcher = (function() {
             var ts = end - start;
 
             var strStartTime = formatDate(new Date(start));
-            if(SYNC_METHOD_UPLOAD == method) {
+            if (SYNC_METHOD_UPLOAD == method) {
                 console.log('sync file end: file length[ ' + fileSize + 'b ]');
             }
 
@@ -314,7 +331,7 @@ var fileWatcher = (function() {
 
 
         });
-        client.on('error', function(err) {
+        client.on('error', function (err) {
             isErr = true
             console.log("\n\n=============\nfatal error\n")
             console.log("* sync file error:[check network or server is start]");
@@ -322,13 +339,25 @@ var fileWatcher = (function() {
         });
 
         client.write('put ' + servfile + '\ncontent-length:' + fileSize + '\n\n');// + content);
-        if(method == SYNC_METHOD_UPLOAD) {
+        if (method == SYNC_METHOD_UPLOAD) {
             var fileStream = fs.createReadStream(localFile);
             fileStream.pipe(client);
         }
 
     }
 
+
+    function sendClean(item) {
+
+        var relativePath = path.relative(item.offline, dir);
+        relativePath = relativePath.replace(/\\/g, '/');
+        var servfile = item.online + '/' + relativePath + '/' + filename;
+        var localFile = dir + '/' + filename;
+
+
+        getConnectClientWithEvent(item, servfile, localFile, 0, SYNC_METHOD_DELETE);
+
+    }
 
 
     //格式化时间戳
@@ -371,13 +400,13 @@ var fileWatcher = (function() {
         if (!item.port) {
             item.port = config.server.port;
         }
-        if(!item.maxFileSize) {
+        if (!item.maxFileSize) {
             item.maxFileSize = config.server.maxFileSize
         }
-        if(1 > item.maxFileSize ) {
+        if (1 > item.maxFileSize) {
             item.maxFileSize = 14
         }
-        item.maxFileSize = item.maxFileSize * 1024*1024;
+        item.maxFileSize = item.maxFileSize * 1024 * 1024;
         item.devSyncAll = argv.devSyncAll;
         item.online = item.online.replace(/\\/g, '/');
         if (item.ignore !== undefined) {
@@ -390,17 +419,19 @@ var fileWatcher = (function() {
         if (item.sh == undefined) {
             item.sh = '';
         }
+        item.beforeCanDelete = argv.canDeleteAll
+        item.endCanExecSh = argv.canExecSh
 
         return item;
     }
 
 
 
-    return function() {
+    return function () {
         var options = process.argv
         var len = options.length;
         var i;
-        var argv = { 'd': '', 'v': '', 'devSyncAll': false, "v": false }
+        var argv = { 'd': '', 'devSyncAll': false, "view": false, "canExecSh": false, "canDeleteAll": false }
 
         for (i = 0; i < len; i++) {
             if (options[i] == '-d') {
@@ -409,6 +440,12 @@ var fileWatcher = (function() {
                 argv.devSyncAll = true;
             } else if (options[i] == "-v") {
                 argv.view = true;
+            } else if (options[i] == "exec") {
+                // synchronize all 表示执行同步同步完成后，执行编译文件
+                argv.canExecSh = true
+            } else if (options[i] == "del") {
+                // synchronize all 表示执行同步前，清理要同步的目录
+                argv.canExecSh = true
             }
         }
 
@@ -438,6 +475,10 @@ var fileWatcher = (function() {
 
                 if (argv.devSyncAll) {
                     printSyncAllHeader(true);
+                    if (argv.canDeleteAll) {
+                        // todo 
+                        sendDelFile("", "", item);
+                    }
                     getSyncFILE(item.offline, item);
                     syncAllStartProcess(item);
                 } else {
--- server/core/buffer.c
+++ server/core/buffer.c
@@ -7,12 +7,10 @@
 
 #include "buffer.h"
 
-
-buffer * buffer_init(pool_t *p)
+buffer *buffer_init(pool_t *p)
 {
 	buffer *b = (buffer *)palloc(p, sizeof(buffer));
-	
-	
+
 	b->ptr = (char *)palloc(p, BUFFER_PIECE_SIZE);
 	b->size = BUFFER_PIECE_SIZE;
 	b->used = 0;
@@ -20,22 +18,23 @@ buffer * buffer_init(pool_t *p)
 	return b;
 }
 
-buffer * buffer_create_size(pool_t *p, size_t len)
+buffer *buffer_create_size(pool_t *p, size_t len)
 {
-	buffer *b =  (buffer *)palloc(p, sizeof(buffer));
+	buffer *b = (buffer *)palloc(p, sizeof(buffer));
 	b->ptr = (char *)palloc(p, len);
 	b->size = len;
 	b->used = 0;
-	
+
 	return b;
 }
 
 void buffer_append_char(buffer *b, char c, pool_t *p)
 {
-	char * ptr;
+	char *ptr;
 
-	if(b->used  == b->size) {
-		int size = b->size<<1;
+	if (b->used == b->size)
+	{
+		int size = b->size << 1;
 		ptr = (char *)palloc(p, size);
 		memcpy(ptr, b->ptr, b->size);
 		b->ptr = ptr;
@@ -43,27 +42,27 @@ void buffer_append_char(buffer *b, char c, pool_t *p)
 	}
 
 	b->ptr[b->used++] = c;
-
 }
 
 void buffer_append_str(buffer *b, char *str, int len, pool_t *p)
 {
-	buffer_prepare_int(p, b, b->used+len);
+	buffer_prepare_int(p, b, b->used + len);
 	memcpy(b->ptr + b->used, str, len);
 	b->used += len;
 }
 
 void buffer_append_n_str(buffer *b, char *str, int len, pool_t *p)
 {
-	buffer_prepare_int(p, b, b->used+len);
+	buffer_prepare_int(p, b, b->used + len);
 	strncat(b->ptr, str, len);
 	b->used += len;
 }
 
-void buffer_append_long(buffer *b, long value, pool_t *p) {
+void buffer_append_long(buffer *b, long value, pool_t *p)
+{
 	char strInt[16];
 	snprintf(strInt, 16, "%ld", value);
-	buffer_append_str(b, strInt, strlen(strInt),p);
+	buffer_append_str(b, strInt, strlen(strInt), p);
 }
 
 int buffer_get_word_with_split(buffer *src, string *dst, char split)
@@ -72,48 +71,74 @@ int buffer_get_word_with_split(buffer *src, string *dst, char split)
 
 	dst->len = 0;
 
-	if(src == NULL || src->ptr == NULL)return -1;
+	if (src == NULL || src->ptr == NULL)
+		return -1;
 	p = src->ptr + src->used;
 	end = p + src->size - 1;
 
-	while(' ' == *p && p != NULL) {if(end == p)return -1;p++;}
+	while (' ' == *p && p != NULL)
+	{
+		if (end == p)
+			return -1;
+		p++;
+	}
 
 	dst->ptr = p;
 
-	for(;*p != split; p++) {
-		if(end == p) {dst->ptr = NULL;return -1;}
+	for (; *p != split; p++)
+	{
+		if (end == p)
+		{
+			dst->ptr = NULL;
+			return -1;
+		}
 	}
 
 	dst->len = p - dst->ptr;
 	src->used += dst->len + 1;
 
-	return 0; 
+	return 0;
 }
 
-
-int buffer_get_line(buffer *src, string * dst)
+int buffer_get_line(buffer *src, string *dst)
 {
 	char *p, *end;
 
 	dst->len = 0;
 
-	if(src == NULL || src->ptr == NULL || src->size == 0){dst->len=0;return -1;}
+	if (src == NULL || src->ptr == NULL || src->size == 0)
+	{
+		dst->len = 0;
+		return -1;
+	}
 	p = src->ptr + src->used;
 	end = src->ptr + src->size - 1;
-	while(' ' == *p && p != NULL) {if(end == p)return -1;p++;}
+	while (' ' == *p && p != NULL)
+	{
+		if (end == p)
+			return -1;
+		p++;
+	}
 
-	if(p >= end) {
+	if (p >= end)
+	{
 		dst->len = 0;
 		return 0;
 	}
 
 	dst->ptr = p;
 
-	for(;*p != '\r' && *p != '\n'; p++)if(end == p){ dst->len = p - dst->ptr + 1; return -1;}
+	for (; *p != '\r' && *p != '\n'; p++)
+		if (end == p)
+		{
+			dst->len = p - dst->ptr + 1;
+			return -1;
+		}
 
 	dst->len = p - dst->ptr;
 	src->used = p - src->ptr + 1;
-	if(p[1] == '\n') src->used += 1;
+	if (p[1] == '\n')
+		src->used += 1;
 
 	return 0;
 }
@@ -125,26 +150,27 @@ void buffer_clear(buffer *b)
 	b->used = 0;
 }
 
-list_buffer_t * list_buffer_create(pool_t *p, size_t size )
+list_buffer_t *list_buffer_create(pool_t *p, size_t size)
 {
-	list_buffer_t * b;
-	
+	list_buffer_t *b;
+
 	b = (list_buffer_t *)palloc(p, sizeof(list_buffer_t));
 	b->size = size;
 	b->head = (list_buffer_item_t *)palloc(p, sizeof(list_buffer_item_t));
-	b->head->ptr  = (char *)palloc(p, size);
+	b->head->ptr = (char *)palloc(p, size);
 	b->head->used = 0;
 	b->foot = b->head;
-	
+
 	return b;
 }
 
-int  list_buffer_add(pool_t *p, list_buffer_t *b)
-{	
-	
-	list_buffer_item_t * item = (list_buffer_item_t *)palloc(p, sizeof(list_buffer_item_t));
-	item->ptr  = (char *)palloc(p, b->size);
-	if(item->ptr == NULL) {
+int list_buffer_add(pool_t *p, list_buffer_t *b)
+{
+
+	list_buffer_item_t *item = (list_buffer_item_t *)palloc(p, sizeof(list_buffer_item_t));
+	item->ptr = (char *)palloc(p, b->size);
+	if (item->ptr == NULL)
+	{
 		return -1;
 	}
 	item->used = 0;
@@ -153,23 +179,24 @@ int  list_buffer_add(pool_t *p, list_buffer_t *b)
 	return 0;
 }
 
-list_buffer_t * list_buffer_last(list_buffer_t *lb)
+list_buffer_t *list_buffer_last(list_buffer_t *lb)
 {
 
 	return lb->foot;
 }
 
-
-
-
 void buffer_n_to_lower(buffer *src, int n)
 {
-	if(n > 0) {
-		if(src != NULL && src->ptr != NULL) {
+	if (n > 0)
+	{
+		if (src != NULL && src->ptr != NULL)
+		{
 			char *p = src->ptr;
-			n = n>src->size?src->size:n;
-			while((p - src->ptr) < n) {
-				if(*p > 'A' && *p < 'Z') {
+			n = n > src->size ? src->size : n;
+			while ((p - src->ptr) < n)
+			{
+				if (*p > 'A' && *p < 'Z')
+				{
 					*p |= 32;
 				}
 				p++;
@@ -178,29 +205,28 @@ void buffer_n_to_lower(buffer *src, int n)
 	}
 }
 
-void buffer_find_str(buffer *src, buffer *dst, char *str) 
+void buffer_find_str(buffer *src, buffer *dst, char *str)
 {
 	char *p;
 
-	if(src != NULL && src->ptr != NULL && src->size > 0 && str != NULL) {
+	if (src != NULL && src->ptr != NULL && src->size > 0 && str != NULL)
+	{
 		p = strstr(src->ptr, str);
-		if(p - src->ptr < src->size) {
+		if (p - src->ptr < src->size)
+		{
 			dst->ptr = p;
 			dst->size = strlen(str);
 		}
-		else {
-			dst->ptr = NULL	;
+		else
+		{
+			dst->ptr = NULL;
 		}
-
 	}
 }
 
-
-
-
-int buffer_prepare_int(pool_t *p, buffer * b, size_t size)
+int buffer_prepare_int(pool_t *p, buffer *b, size_t size)
 {
-	if((0 == b->size) ||
+	if ((0 == b->size) ||
 		(size > b->size))
 	{
 		char *preValue = b->ptr;
@@ -212,73 +238,78 @@ int buffer_prepare_int(pool_t *p, buffer * b, size_t size)
 	return 0;
 }
 
-
 int buffer_to_lower(buffer *b)
 {
-	char * c ;
-	if(0 == b->used) return 0;
+	char *c;
+	if (0 == b->used)
+		return 0;
 
-	for(c = b->ptr; c-b->ptr <= b->used; c++)
+	for (c = b->ptr; c - b->ptr <= b->used; c++)
 	{
-		if (*c >= 'A' && *c <= 'Z') 
+		if (*c >= 'A' && *c <= 'Z')
 		{
 			*c |= 32;
 		}
 	}
-	
+
 	return 0;
 }
 
 int buffer_to_upper(buffer *b)
 {
-	char * c ;
+	char *c;
 
-	if(0 == b->used) return 0;
+	if (0 == b->used)
+		return 0;
 
-	for(c = b->ptr; *c; c++)
+	for (c = b->ptr; *c; c++)
 	{
-		if (*c >= 'A' && *c <= 'Z') 
+		if (*c >= 'A' && *c <= 'Z')
 		{
-			*c &= 223;  // *c &= ~32;
+			*c &= 223; // *c &= ~32;
 		}
 	}
-	
+
 	return 0;
 }
 
-
-int buffer_is_equal(buffer *a, buffer *b) 
+int buffer_is_equal(buffer *a, buffer *b)
 {
-	if (a->used != b->used) return 0;
+	if (a->used != b->used)
+		return 0;
 
 	return (0 == strcmp(a->ptr, b->ptr));
 }
 
-int buffer_caseless_compare(buffer *a, buffer *b) 
+int buffer_caseless_compare(buffer *a, buffer *b)
 {
-	if (a->used != b->used) return 0;
-	if (0 == a->used && 0 == b->used) return 1;
+	if (a->used != b->used)
+		return 0;
+	if (0 == a->used && 0 == b->used)
+		return 1;
 
 	return (0 == strcasecmp(a->ptr, b->ptr));
 }
 
-int buffer_is_equal_len(buffer *a, buffer *b, size_t len) 
+int buffer_is_equal_len(buffer *a, buffer *b, size_t len)
 {
-	if (a->used != b->used) return 0;
-	if (0 == a->used && 0 == b->used) return 1;
+	if (a->used != b->used)
+		return 0;
+	if (0 == a->used && 0 == b->used)
+		return 1;
 
 	return (0 == strncmp(a->ptr, b->ptr, len));
 }
 
-int buffer_caseless_compare_len(buffer *a, buffer *b) 
+int buffer_caseless_compare_len(buffer *a, buffer *b)
 {
-	if (a->used != b->used) return 0;
-	if (0 == a->used && 0 == b->used) return 1;
+	if (a->used != b->used)
+		return 0;
+	if (0 == a->used && 0 == b->used)
+		return 1;
 
 	return (0 == strncasecmp(a->ptr, b->ptr, a->used));
 }
-
-
 
 int buffer_path_simplify(buffer *dest, const buffer *src)
 {
@@ -291,57 +322,64 @@ int buffer_path_simplify(buffer *dest, const buffer *src)
 		out将要写入的结果的位置
 	*/
 	char *start, *slash, *walk, *out;
-	
+
 	char pre; //当前匹配的前一个字符
 
-	if (src == NULL ||  dest == NULL)
+	if (src == NULL || dest == NULL)
 		return -1;
 
-
-	walk  = src->ptr;
+	walk = src->ptr;
 	start = dest->ptr;
-	out   = dest->ptr;
+	out = dest->ptr;
 	slash = dest->ptr;
 
-
-
-	while (*walk == ' ') {
+	while (*walk == ' ')
+	{
 		walk++;
 	}
 
 	pre = *(walk++);
-	c    = *(walk++);
-	if (pre != '/') {
+	c = *(walk++);
+	if (pre != '/')
+	{
 		*(out++) = '/';
 	}
 	*(out++) = pre;
 
-	if (pre == '\0') {
+	if (pre == '\0')
+	{
 		*out = 0;
 		return 0;
 	}
 
-	while (1) {
+	while (1)
+	{
 		// '/'表示本次匹配的结束，'\0'表示整个过程的结束
-		if (c == '/' || c == '\0') {
+		if (c == '/' || c == '\0')
+		{
 			//计算本次共匹配到的字符 即使'/'与'/'的距离
 			count = out - slash;
 			//说明本次匹配为../需要删除前面的目录
-			if (count == 3 && pre == '.') {
-					
+			if (count == 3 && pre == '.')
+			{
+
 				//首先回到本次匹配的开始，即使../的一个点的位置，
 				out = slash;
 				//回溯到前面的一个目录，只要前面不是根目录
-				if (out > start) {
+				if (out > start)
+				{
 					out--;
-					while (out > start && *out != '/') {
+					while (out > start && *out != '/')
+					{
 						out--;
 					}
 				}
 
 				if (c == '\0')
 					out++;
-			} else if (count == 1 || pre == '.') {
+			}
+			else if (count == 1 || pre == '.')
+			{
 				//本次匹配为“./”只要删除掉就可。即使回到本次开始匹配的位置
 				out = slash;
 				if (c == '\0')
@@ -355,7 +393,7 @@ int buffer_path_simplify(buffer *dest, const buffer *src)
 			break;
 		//先将内容写入，本保存上一个的内容
 		pre = c;
-		c    = *walk;
+		c = *walk;
 		*out = pre;
 
 		out++;
@@ -366,4 +404,3 @@ int buffer_path_simplify(buffer *dest, const buffer *src)
 	dest->used = (out - start);
 	return 0;
 }
-
